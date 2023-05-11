b17cc87764dd660c9c9fcfaaa0b2114e3ea5df46
Reyner Veliz <reynerv@yahoo.com>
2023-05-10 23:46:11 -0600

    Controlled vs Uncontrolled Components

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,27 +1,15 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
-import { useState } from 'react';
 
 const ExpenseItem = props => {
-    // useState returns an array.  First element is the variable, second is a function.
-
-    const [title, setTitle] = useState(props.title);
-    console.log("ExpenseItem evaluated by React");
-
-    const clickHandler = () => {
-        setTitle('updated!')
-        console.log(title);
-    }
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
