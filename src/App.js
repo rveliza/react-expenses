6c1d571a7bdf1aaefb4d47f780c47fce309311f2
Reyner Veliz <reyner@reynerveliz.com>
2023-05-12 10:57:10 -0600

    Filter lists

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ const DUMMY_EXPENSES = [
     id: 'e2',
     title: 'Toilet Paper',
     amount: 94.12,
-    date: new Date(2022, 7, 14)
+    date: new Date(2023, 7, 14)
   },
   {
     id: 'e3',
     title: 'New TV',
     amount: 799.49,
-    date: new Date(2021, 2, 12)
+    date: new Date(2024, 2, 12)
   },
   {
     id: 'e4',
     title: 'New Desk(Wooden)',
     amount: 450,
-    date: new Date(2021, 5, 12)
+    date: new Date(2022, 5, 12)
   }
 ]
 
