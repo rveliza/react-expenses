97fbd6922fe67c325582d96d3c458b8b2028d386
Reyner Veliz <reynerv@yahoo.com>
2023-05-18 18:58:46 -0600

    Adding dynamic styles

--- src/components/Chart/ChartBar.jsx
+++ src/components/Chart/ChartBar.jsx
@@ -1,5 +1,17 @@
 const ChartBar = props => {
+    let barFillHeight = '10%';
+    if(props.max >0){
+        barFillHeight = Math.round((props.value / props.maxValue) * 100) + '10%'
+    }
 
+    return (
+        <div className="chart-bar">
+            <div className="chart-bar__inner">
+                <div className="chart-bar__fill" style={{height: barFillHeight}}></div>
+            </div>
+            <div className="chat-bar__label">{props.label}</div>
+        </div>
+    );
 }
 
 export default ChartBar;
